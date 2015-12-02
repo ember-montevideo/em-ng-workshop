ddf0cb16b0fd4cb00739d56b27891e99f43f3df3
Adrian Mugnolo <adrian@mugnolo.com>
2015-12-01 22:06:02 -0300

    [em] Show each album

--- em-music/app/router.js
+++ em-music/app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('albums');
-  this.route('album');
+  this.route('album', {
+    path: ':album_id'
+  });
 });
 
 export default Router;
