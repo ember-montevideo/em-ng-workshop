ade7a311efc24db1130e13b18e455741a95d6c8e
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2015-12-02 20:00:00 -0300

    [ng] Add root route

--- ng-music/src/app/music.ts
+++ ng-music/src/app/music.ts
@@ -1,11 +1,18 @@
 import {Component, bootstrap} from 'angular2/angular2';
 import {AlbumsPage} from './components/albums-page/albums-page';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
+@RouteConfig([
+  {
+    path: '/',
+    component: AlbumsPage
+  }
+])
 @Component({
   selector: 'music-app',
   providers: [],
   templateUrl: 'app/music.html',
-  directives: [AlbumsPage],
+  directives: [ROUTER_DIRECTIVES],
   pipes: []
 })
 export class MusicApp {
