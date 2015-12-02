0ce10939cccbf7fc1f9545f3fefa3fe254048522
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2015-12-02 18:44:21 -0300

    [ng] Create base project

---
+++ ng-music/src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/angular2';
+import {MusicApp} from './app/music';
+
+
+bootstrap(MusicApp);
---
+++ ng-music/src/app/music.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'music-app',
+  providers: [],
+  templateUrl: 'app/music.html',
+  directives: [],
+  pipes: []
+})
+export class MusicApp {
+
+  constructor() {}
+
+}
